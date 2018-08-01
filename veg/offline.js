d3c27960a73cc3bd1050946fa8f522fac2c8060d
rajputsushil <sushil.rajput.jbp@gmail.com>
2018-08-01 19:18:12 +0530

    new game vegemite
    vegemite game

---
+++ veg/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1533131025,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/image-sheet0.png",
+		"images/alternativebox-sheet0.png",
+		"images/background2-sheet0.png",
+		"images/background-sheet0.png",
+		"images/nekostart-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/launchfireworks.png",
+		"images/ptimer.png",
+		"images/star2-sheet0.png",
+		"images/play-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/emo-sheet0.png",
+		"images/emo-sheet1.png",
+		"images/emo-sheet2.png",
+		"media/good.ogg",
+		"media/wait.ogg",
+		"media/wrong.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
